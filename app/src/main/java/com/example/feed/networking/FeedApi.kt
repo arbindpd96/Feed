f5b7b25aa8bafa1e05525bd66f5d3f955786f38e
Arbind <arbindprasad096@gmaail.com>
2021-08-31 12:22:59 +0530

    api correction

--- app/src/main/java/com/example/feed/networking/FeedApi.kt
+++ app/src/main/java/com/example/feed/networking/FeedApi.kt
@@ -4,10 +4,12 @@ package com.example.feed.networking
 import com.example.feed.modals.request.FeedRequest
 import com.example.feed.modals.response.FeedResponse
 import com.example.feed.networking.StandardResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface FeedApi {
-    @GET("/PinkDelivery/dev/api/product/get")
-    suspend fun getFeedData(feedRequest : FeedRequest): StandardResponse<FeedResponse>
+    @POST("/PinkDelivery/dev/api/product/get")
+    suspend fun getFeedData(@Body feedRequest : FeedRequest): StandardResponse<FeedResponse>
 }
